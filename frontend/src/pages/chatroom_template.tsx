1b35181e30b943bdeb87d96a1ac3cd75e6d71597
KawinM23 <kawin.m235@gmail.com>
2023-04-30 17:02:36 +0700

    Add chatscope package

--- frontend/src/pages/chatroom_template.tsx
+++ frontend/src/pages/chatroom_template.tsx
@@ -1,9 +1,50 @@
+import styles from "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
+import {
+  MainContainer,
+  ChatContainer,
+  MessageList,
+  Message,
+  MessageInput,
+  Conversation,
+  ConversationHeader,
+} from "@chatscope/chat-ui-kit-react";
+import { useRouter } from "next/router";
+
 type ChatProps = {
   cycleTheme: any;
 };
 
 const ChatDemo: Function = (props: ChatProps) => {
-  return <div className="my-10 mx-auto w-[80vw]"></div>;
+  const router = useRouter();
+
+  return (
+    <div style={styles} className="h-[100vh]">
+      <MainContainer>
+        <ChatContainer>
+          <ConversationHeader>
+            <ConversationHeader.Back
+              onClick={() => {
+                router.push("/");
+              }}
+            />
+            <ConversationHeader.Content userName={"Joe"} />
+          </ConversationHeader>
+          <MessageList>
+            <Message
+              model={{
+                message: "Hello my friend",
+                sentTime: "just now",
+                sender: "Joe",
+              }}
+            >
+              <Message.Header sender="Joe" />
+            </Message>
+          </MessageList>
+          <MessageInput attachButton={false} placeholder="Type message here" />
+        </ChatContainer>
+      </MainContainer>
+    </div>
+  );
 };
 
 export default ChatDemo;
