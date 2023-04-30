f945200977ec5e07f251d95ca73407b545005a9d
Thanadol Chitthamlerd <thanadolps@gmail.com>
2023-04-30 14:18:25 +0700

    Add db code

---
+++ python_code/db/db.py
@@ -0,0 +1,26 @@
+from pony.orm import *
+
+
+db = Database()
+
+# Define entities in database
+
+
+class ChatRoom(db.Entity):
+    id = PrimaryKey(int, auto=True)
+    name = Required(str)
+    massages = Set('Massage')
+
+
+class Massage(db.Entity):
+    id = PrimaryKey(int, auto=True)
+    massage_no = Required(int)
+    sender = Required(str)
+    chat_room = Required(ChatRoom)
+
+# Connet database
+
+
+db.bind(provider='sqlite', filename='db.sqlite', create_db=True)
+# set_sql_debug(True)
+db.generate_mapping(create_tables=True)
