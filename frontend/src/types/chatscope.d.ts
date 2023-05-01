5d1ebe2322061bbfb52b1d84ac8ab64bb1b7e00d
Thanadol Chitthamlerd <thanadolps@gmail.com>
2023-05-02 03:07:41 +0700

    Resolve chatscope style typescript error

---
+++ frontend/src/types/chatscope.d.ts
@@ -0,0 +1,6 @@
+declare module "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css" {
+  import type { CSSProperties } from "react";
+
+  const styles: CSSProperties;
+  export default styles;
+}
