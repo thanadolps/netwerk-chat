a5eeee4e42a086ab7eb67f965bcbe3a5972a7011
Tuchtham Sungkameka <touch0869@gmail.com>
2023-05-03 09:01:59 +0700

    try to fix with try loop and await async

--- python_code/client.py
+++ python_code/client.py
@@ -1,107 +1,132 @@
 import asyncio
 import socketio
+import time
 
-from Utils import cmd,var,req,res
+from Utils import cmd, var, req, res
 
 sio = socketio.Client(
-    # logger=True,
-    )
+    logger=True,
+)
+
 
 @sio.event
 def connect():
     print('connection established')
     sio.emit(cmd.message, 'hello from python')
-    
+
     msg = {
-        var.data : 'use dict to send to default group'
-        }
+        var.data: 'use dict to send to default group'
+    }
     sio.emit(cmd.message, msg)
-    msg = {var.data:'takeshi'}
+    msg = {var.data: 'takeshi'}
     sio.emit(cmd.change_user_name, msg)
     msg = {
-        var.group_name : 'default',
-        var.data : 'rename and send',
-        }
+        var.group_name: 'default',
+        var.data: 'rename and send',
+    }
     sio.emit(cmd.message, msg)
     msg = {
-        var.group_name : 'new group',
-        var.data : 'create group and send',
-        }
+        var.group_name: 'new group',
+        var.data: 'create group and send',
+    }
     sio.emit(cmd.create_group, 'new group')
     sio.emit(cmd.message, msg)
+
 
 @sio.event
 def message(data):
     out = f'{data[var.sender]} (error) : {data[var.data]}'
     print(out)
+
+
 @sio.on(cmd.message)
 def display_msg(data):
     out = f'{data[var.sender]} ({data[var.group_name]}) : {data[var.data]}'
     print(out)
 
+
 @sio.on(cmd.error)
 def display_error(data):
     out = data[var.data]
     print(out)
-    
+
+
 @sio.on(cmd.response)
 def display_error(data):
-    out = data#[var.data]
+    out = data  # [var.data]
     print(out)
-    
+
+
 @sio.event
 def disconnect():
     print('disconnected from server')
 
-def main():
+
+async def main():
     group = var.default_group_name
-    sio.connect('http://localhost:5069')
+
+    # await sio.connect('http://localhost:5069')
+
+    connected = False
+    while not connected:
+        try:
+            await sio.connect("http://localhost:5069")
+            print("Socket established")
+            connected = True
+        except Exception as ex:
+            print("Failed to establish initial connnection to server:", type(ex).__name__)
+            time.sleep(2)
+
     while 1:
         s = input()
         sl = s.split()
         if 'help' in sl:
             print(HELP)
-        elif sl[0] in ['ls','list']:
-            if sl[1]== 'group_name':    r = req.group_name
-            if sl[1]== 'user_name':     r = req.user_name
+        elif sl[0] in ['ls', 'list']:
+            if sl[1] == 'group_name':
+                r = req.group_name
+            if sl[1] == 'user_name':
+                r = req.user_name
             # if sl[1]== 'db':            r = req.db
-            if sl[1]== 'chat_hist':     r = req.chat_hist
-            msg = {var.group_name:group,var.data:r}
+            if sl[1] == 'chat_hist':
+                r = req.chat_hist
+            msg = {var.group_name: group, var.data: r}
             sio.emit(cmd.request, msg)
         elif sl[0] == 'change_name':
-            new_name=' '.join(sl[1:])
-            msg = {var.data:new_name}
+            new_name = ' '.join(sl[1:])
+            msg = {var.data: new_name}
             sio.emit(cmd.change_user_name, new_name)
         elif sl[0] == 'change_group_name':
-            new_name=' '.join(sl[1:])
-            msg = {var.group_name:group_name,var.data:new_name}
+            new_name = ' '.join(sl[1:])
+            msg = {var.group_name: group_name, var.data: new_name}
             sio.emit(cmd.change_group_name, msg)
             group = group_name
         elif sl[0] == 'join_group':
-            group_name=' '.join(sl[1:])
-            msg = {var.group_name:group_name}
+            group_name = ' '.join(sl[1:])
+            msg = {var.group_name: group_name}
             sio.emit(cmd.join_group, msg)
             group = group_name
         elif sl[0] == 'create_group':
-            group_name=' '.join(sl[1:])
-            msg = {var.group_name:group_name}
+            group_name = ' '.join(sl[1:])
+            msg = {var.group_name: group_name}
             sio.emit(cmd.create_group, msg)
             group = group_name
         elif sl[0] == 'leave_group':
-            group_name=' '.join(sl[1:])
-            msg = {var.group_name:group_name}
+            group_name = ' '.join(sl[1:])
+            msg = {var.group_name: group_name}
             sio.emit(cmd.leave_group, msg)
             group = var.default_group_name
-        elif sl[0] == 'group': # group new group
-            group_name=' '.join(sl[1:])
+        elif sl[0] == 'group':  # group new group
+            group_name = ' '.join(sl[1:])
             group = group_name
-        elif sl[0] == 'dm': # dm user1 hello
-            msg = {var.reciever:sl[1],var.data:' '.join(sl[2:])}
+        elif sl[0] == 'dm':  # dm user1 hello
+            msg = {var.reciever: sl[1], var.data: ' '.join(sl[2:])}
             sio.emit(cmd.dm, msg)
         else:
-            msg = {var.group_name:group,var.data:s}
+            msg = {var.group_name: group, var.data: s}
             sio.emit(cmd.message, msg)
     sio.wait()
+
 
 HELP = '''
 to join group: join_group <group_name>
